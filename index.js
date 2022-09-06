bc18e50bbcd54a05eac2e299c8b39807f46b03e1
xvirusdeadx <naza46721801ivon@gmail.com>
2022-09-05 22:33:54 -0300

    calculadora

---
+++ index.js
@@ -0,0 +1,31 @@
+function limpiar() {
+    document.getElementById('formulario').reset();
+    
+}
+
+
+function sumar() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+document.getElementById('el-resultado').innerHTML = x+y; 
+}
+
+function restar() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+document.getElementById('el-resultado').innerHTML = x-y; 
+}
+
+
+function multiplicar() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+document.getElementById('el-resultado').innerHTML = x*y; 
+}
+
+
+function dividir() {
+    var x = parseInt(document.getElementById('valor1').value);
+    var y = parseInt(document.getElementById('valor2').value);
+document.getElementById('el-resultado').innerHTML = x/y; 
+}
